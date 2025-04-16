e4459ac3723d7223fb4a2385e8c8eb5429c91cab
IrhamBae <new email>
2025-04-16 22:06:15 +0700

    Merefactor konstruktor Employee dengan mengurangi jumlah parameter melalui pengelompokan data

--- TaxCalculator/src/lib/Employee.java
+++ TaxCalculator/src/lib/Employee.java
@@ -1,117 +1,113 @@
 package lib;
 
 import java.time.LocalDate;
-import java.time.Month;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Employee {
 
-	private String employeeId;
-	private String firstName;
-	private String lastName;
-	private String idNumber;
-	private String address;
-	
-	private int yearJoined;
-	private int monthJoined;
-	private int dayJoined;
-	private int monthWorkingInYear;
-	
-	private boolean isForeigner;
-	private boolean gender; //true = Laki-laki, false = Perempuan
-	
-	private int monthlySalary;
-	private int otherMonthlyIncome;
-	private int annualDeductible;
-	
-	private String spouseName;
-	private String spouseIdNumber = "";
+    private String employeeId;
+    private PersonalData personalData;
+    private JoiningDate joiningDate;
+    private boolean isForeigner;
+    private Gender gender;
 
-	private List<String> childNames;
-	private List<String> childIdNumbers;
-	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
-		this.employeeId = employeeId;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.idNumber = idNumber;
-		this.address = address;
-		this.yearJoined = yearJoined;
-		this.monthJoined = monthJoined;
-		this.dayJoined = dayJoined;
-		this.isForeigner = isForeigner;
-		this.gender = gender;
-		
-		childNames = new LinkedList<String>();
-		childIdNumbers = new LinkedList<String>();
-	}
-	
-	/**
-	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
-	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
-	 */
-	
-	public void setMonthlySalary(int grade) {
-		int baseSalary;
+    private int monthWorkingInYear;
 
-		switch (grade) {
-			case 1:
-				baseSalary = 3000000;
-				break;
-			case 2:
-				baseSalary = 5000000;
-				break;
-			case 3:
-				baseSalary = 7000000;
-				break;
-			default:
-				baseSalary = 0;
-		}
+    private int monthlySalary;
+    private int otherMonthlyIncome;
+    private int annualDeductible;
 
-		if (isForeigner) {
-			baseSalary *= 1.5;
-		}
+    private String spouseName;
+    private String spouseIdNumber = "";
 
-		monthlySalary = baseSalary;
-	}
-	
-	public void setAnnualDeductible(int deductible) {	
-		this.annualDeductible = deductible;
-	}
-	
-	public void setAdditionalIncome(int income) {	
-		this.otherMonthlyIncome = income;
-	}
-	
-	public void setSpouse(String spouseName, String spouseIdNumber) {
-		this.spouseName = spouseName;
-		this.spouseIdNumber = idNumber;
-	}
-	
-	public void addChild(String childName, String childIdNumber) {
-		childNames.add(childName);
-		childIdNumbers.add(childIdNumber);
-	}
-	
-	public int getAnnualIncomeTax() {
-		
-		//Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
-		LocalDate date = LocalDate.now();
-		
-		if (date.getYear() == yearJoined) {
-			monthWorkingInYear = date.getMonthValue() - monthJoined;
-		}else {
-			monthWorkingInYear = 12;
-		}
-		
-		return TaxFunction.calculateTax(
-			    monthlySalary, 
-			    otherMonthlyIncome, 
-			    monthWorkingInYear, 
-			    annualDeductible, 
-			    spouseIdNumber != null && !spouseIdNumber.equals(""), 
-			    childIdNumbers.size()
-			);
-	}
+    private List<String> childNames;
+    private List<String> childIdNumbers;
+
+    public Employee(String employeeId, PersonalData personalData, JoiningDate joiningDate, boolean isForeigner, Gender gender) {
+        this.employeeId = employeeId;
+        this.personalData = personalData;
+        this.joiningDate = joiningDate;
+        this.isForeigner = isForeigner;
+        this.gender = gender;
+
+        this.childNames = new LinkedList<>();
+        this.childIdNumbers = new LinkedList<>();
+    }
+
+    public void setMonthlySalary(int grade) {
+        int baseSalary;
+        switch (grade) {
+            case 1:
+                baseSalary = 3000000;
+                break;
+            case 2:
+                baseSalary = 5000000;
+                break;
+            case 3:
+                baseSalary = 7000000;
+                break;
+            default:
+                baseSalary = 0;
+        }
+
+        if (isForeigner) {
+            baseSalary *= 1.5;
+        }
+
+        this.monthlySalary = baseSalary;
+    }
+
+    public void setAnnualDeductible(int deductible) {
+        this.annualDeductible = deductible;
+    }
+
+    public void setAdditionalIncome(int income) {
+        this.otherMonthlyIncome = income;
+    }
+
+    public void setSpouse(String spouseName, String spouseIdNumber) {
+        this.spouseName = spouseName;
+        this.spouseIdNumber = spouseIdNumber;
+    }
+
+    public void addChild(String childName, String childIdNumber) {
+        childNames.add(childName);
+        childIdNumbers.add(childIdNumber);
+    }
+
+    public int getAnnualIncomeTax() {
+        LocalDate now = LocalDate.now();
+        if (now.getYear() == joiningDate.getDateJoined().getYear()) {
+            monthWorkingInYear = now.getMonthValue() - joiningDate.getDateJoined().getMonthValue();
+        } else {
+            monthWorkingInYear = 12;
+        }
+
+        return TaxFunction.calculateTax(
+                monthlySalary,
+                otherMonthlyIncome,
+                monthWorkingInYear,
+                annualDeductible,
+                spouseIdNumber != null && !spouseIdNumber.isEmpty(),
+                childIdNumbers.size()
+        );
+    }
+
+    // Optional: Getter untuk testing atau akses luar
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public PersonalData getPersonalData() {
+        return personalData;
+    }
+
+    public JoiningDate getJoiningDate() {
+        return joiningDate;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
 }
